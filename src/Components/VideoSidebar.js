6fef21219527c5e16565e51947173a776082f92d
Wesley <wspencer3295@yahoo.com>
2021-05-11 12:34:11 -0700

    vidSidebar component

---
+++ src/Components/VideoSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CSS/VideoSidebar.css'
+
+const VideoSidebar = () => {
+    return (
+        <div className="videoSidebar">
+            
+        </div>
+    )
+}
+
+export default VideoSidebar
