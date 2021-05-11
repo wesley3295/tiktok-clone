5600a5fd9e311c3df442a27bf79886c7257e3d00
Wesley <wspencer3295@yahoo.com>
2021-05-11 12:32:49 -0700

    import components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Video from './Components/Video'
+import VideoSidebar from './Components/VideoSidebar'
+import VideoFooter from './Components/VideoFooter'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* App Container */}
+      <div className="app__videos">
+        <Video />
+        <Video />
+      <VideoFooter />
+      </div>
+      <VideoSidebar />
     </div>
   );
 }
