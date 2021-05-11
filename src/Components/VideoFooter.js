2f5caed8e13dbbee6a222ed37666ac66763d6322
Wesley <wspencer3295@yahoo.com>
2021-05-11 12:33:55 -0700

    vidFooter component

---
+++ src/Components/VideoFooter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './CSS/VideoFooter.css'
+
+const VideoFooter = () => {
+    return (
+        <div className="videoFooter">
+          <div className="videoFooter__text">
+              <h3>@wes3295</h3>
+              <p>This is some description</p>
+              </div>  
+        </div>
+    )
+}
+
+export default VideoFooter
